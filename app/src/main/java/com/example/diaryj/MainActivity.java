e4f1487070861c984da841b993b07c56f669e494
astokarev <sandrotokarev@gmail.com>
2021-09-02 18:30:59 +0300

    Save button is disabled until EditText clicked

--- app/src/main/java/com/example/diaryj/MainActivity.java
+++ app/src/main/java/com/example/diaryj/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.diaryj;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -10,26 +8,37 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity implements OnClickListener {
     EditText editText;
     Button button;
-    LinearLayout llMain;
-    int wrapContent = LinearLayout.LayoutParams.WRAP_CONTENT;
+    LinearLayout noteList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editText = findViewById(R.id.editText);
         button = findViewById(R.id.button);
-        llMain = findViewById(R.id.llMain);
+        button.setEnabled(false);
+        noteList = findViewById(R.id.noteList1);
         button.setOnClickListener(this);
-        LinearLayout llMain = new LinearLayout(this);
-        TextView gen = new TextView(this);
+        editText.setOnClickListener(this);
     }
+
     @Override
     public void onClick(View v) {
-        TextView gen = new TextView(this);
-        llMain.addView(gen);
-        gen.setText(editText.getText());
+        switch (v.getId()) {
+            case R.id.button:
+                TextView note = new TextView(this);
+                Date date = new Date();
+                noteList.addView(note);
+                note.setText(date.toString()+" "+ editText.getText());
+            case R.id.editText:
+                button.setEnabled(true);
+        }
     }
 }
